51f0bbf5176a9679f42517382b1d791e06912a6f
RizkyPutra151 <rzkyptra151@gmail.com>
2025-08-28 10:27:47 +0700

    Projct game

---
+++ Soapy/Assets/Tamplated/Script/Player/Player Move.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed= 5f;
+    public float jumpForce = 10f;
+    public float speedIncreaseAmount = 1f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        PointManager.OnScoreThresholdReached += IncreaseSpeed;
+        
+    }
+    void OnDestroy()
+    {
+       
+        PointManager.OnScoreThresholdReached -= IncreaseSpeed;
+    }
+
+    void Update()
+    {
+
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+            SFXManager.instance.PlaySFX("Jump");
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isGrounded = false;
+    }
+    
+    private void IncreaseSpeed(int currentScore)
+    {
+        moveSpeed += speedIncreaseAmount;
+        Debug.Log("Speed increased! New speed: " + moveSpeed);
+    }
+   
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
